336c061da7f91c5313b5635a1378d32294641562
farhanzaidann <farhnzaidan@gmail.com>
2024-12-03 15:26:07 +0700

    feature: add variable when order ticket

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ class Event:
         self.deskripsi = deskripsi
         self.stok = stok
 
-
 class Tiket:
-    def __init__(self, event, jumlah):
+    def __init__(self, event, jumlah, nama, no_hp, email):
         self.event = event
         self.jumlah = jumlah
+        self.nama = nama
+        self.no_hp = no_hp
+        self.email = email
 
 class AplikasiPemesanan:
     def __init__(self):
@@ -30,6 +32,7 @@ class AplikasiPemesanan:
         for i, event in enumerate(self.events):
             print(f"{i + 1:<5} {event.nama:<25} {event.tanggal:<15} {event.harga:<10} {event.stok:<10}")
         print("=" * 70)
+        print(" ")
 
     def pesan_tiket(self):
         self.tampilkan_event()
@@ -38,14 +41,20 @@ class AplikasiPemesanan:
             if 0 <= pilihan < len(self.events):
                 jumlah = int(input("Jumlah tiket yang ingin dipesan: "))
                 event = self.events[pilihan]
+                nama = input("Nama Pemesan: ")
+                no_hp = int(input("No HP Pemesan: "))
+                email = input("Email Pemesan: ")
+                harga = event.harga * jumlah
+                print("=" * 70)
 
                 if jumlah > event.stok:
                     print(f"Stok tidak mencukupi. Tersedia hanya {event.stok} tiket.")
                 else:
-                    tiket = Tiket(event, jumlah)
+                    tiket = Tiket(event, jumlah, nama, no_hp, email)
                     self.pemesanan.append(tiket)
                     event.stok -= jumlah  # Mengurangi stok tiket
-                    print(f"Tiket untuk {event.nama} sebanyak {jumlah} telah dipesan.")
+                    print(f"Tiket untuk {event.nama} sebanyak {jumlah} telah dipesan oleh:")
+                    print(f"Nama: {nama}\nNo HP: {no_hp}\nEmail : {email}\nHarga: Rp.{harga}")
             else:
                 print("Pilihan tidak valid.")
         except ValueError:
@@ -53,15 +62,19 @@ class AplikasiPemesanan:
 
     def tampilkan_pemesanan(self):
         print("\n" + "=" * 70)
-        print("Daftar Pemesanan:")
+        print("Struk Tiket:")
         print("=" * 70)
         if not self.pemesanan:
             print("Tidak ada pemesanan.")
             print("=" * 70)
             return
-        print(f"{'No.':<5} {'Nama Event':<25} {'Jumlah':<10} {'Total Harga':<15}")
-        print("=" * 70)
+
         for i, tiket in enumerate(self.pemesanan):
             total_harga = tiket.jumlah * tiket.event.harga
-            print(f"{i + 1:<5} {tiket.event.nama:<25} {tiket.jumlah:<10} {total_harga:<15}")
-        print("=" * 70)
+            print(f"{'Nama Event:':<15} {tiket.event.nama}")
+            print(f"{'Jumlah:':<15} {tiket.jumlah}")
+            print(f"{'Total Harga:':<15} {total_harga}")
+            print(f"{'Nama Pemesan:':<15} {tiket.nama}")  
+            print(f"{'No HP:':<15} {tiket.no_hp}")
+            print(f"{'Email:':<15} {tiket.email}")
+            print("=" * 70)
